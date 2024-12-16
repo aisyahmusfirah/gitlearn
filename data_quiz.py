10263197fe53692703d0129bcbd615f6d81367b7
Aisyah Musfirah <aisyah.123450084@student.itera.ac.id>
2024-12-16 23:12:34 +0700

    Add files via upload

---
+++ data_quiz.py
@@ -0,0 +1,34 @@
+colors = {
+    "dark_purple": "#4B244A",
+    "orange": "#F7941D",
+    "cream": "#FEEFC3",
+    "black": "#000000",
+    "white": "#FFFFFF",
+}
+
+users = {}
+questions = [
+    {"question": "Survey", "answer": False},
+    {"question": "Praktek", "answer": False},
+    {"question": "Frekuensi", "answer": True},
+    {"question": "Nafas", "answer": False},
+    {"question": "Objek", "answer": True},
+    {"question": "Fondasi", "answer": True},
+    {"question": "Atmosfir", "answer": False},
+    {"question": "Karier", "answer": True},
+    {"question": "Aktifitas", "answer": False},
+    {"question": "Apotek", "answer": True}
+]
+
+treats = [
+    "Kelarin semua tugas sebelum jam 6 sore!",
+    "Bilang i love you ke ibumu.",
+    "Streaming video tentang productivity min. 15 menit.",
+    "Jangan beli gorengan selama 3 hari",
+    "Luangkan 10 menit untuk membaca buku.",
+    "Sedekah untuk Palestina min. 10k.",
+    "Share di status WA postingan tentang Palestina.",
+    "Doakan keselamatan dan kedamaian untuk Palestina hari ini.",
+    "Beliin bestiemu makan siang.",
+    "Baca kitab suci min. 1 halaman.",
+]
